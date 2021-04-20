f0c6787d860afb0f557ef9dae2d92c6cbfdd873a
Diego Rodriguez <=>
2021-04-19 22:03:19 -0300

    unauthorizedError

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,16 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import passport from "passport";
 import "../auth/passportHandler";
 
-export class AuthController {
+export const unauthorizedError = { status: "error", code: "unauthorized" };
 
+export class AuthController {
   public authenticateJWT(req: Request, res: Response, next: NextFunction) {
     passport.authenticate("jwt", function (err, user, info) {
       if (err) {
         console.log(err);
-        return res.status(401).json({ status: "error", code: "unauthorized" });
+        return res.status(401).json(unauthorizedError);
       }
       if (!user) {
-        return res.status(401).json({ status: "error", code: "unauthorized" });
+        return res.status(401).json(unauthorizedError);
       } else {
         return next();
       }
@@ -22,23 +23,21 @@ export class AuthController {
     passport.authenticate("jwt", function (err, user, jwtToken) {
       if (err) {
         console.log(err);
-        return res.status(401).json({ status: "error", code: "unauthorized" });
+        return res.status(401).json(unauthorizedError);
       }
       if (!user) {
-        return res.status(401).json({ status: "error", code: "unauthorized" });
+        return res.status(401).json(unauthorizedError);
       } else {
         const scope = req.baseUrl.split("/").slice(-1)[0];
         const authScope = jwtToken.scope;
         if (authScope && authScope.indexOf(scope) > -1) {
           return next();
-        }
-        else {
-          return res.status(401).json({ status: "error", code: "unauthorized" });
+        } else {
+          return res
+            .status(401)
+            .json(unauthorizedError);
         }
       }
     })(req, res, next);
   }
-
-
 }
-
